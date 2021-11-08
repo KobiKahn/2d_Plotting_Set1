6cb5ebe6908a0c65d4b643c2d7f597efb070b055
kobi <kobikahn@gmail.com>
2021-11-08 14:46:35 -0500

    finish 1

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
+import matplotlib.pyplot as plt
 
+def open_file(file_name):
+    x_counter = 0
+    x = []
+    y = []
+
+    with open(file_name) as file:
+
+        for row in file:
+            row = row.split()
+            x_counter += 1
+            if x_counter == 1:
+
+                xlabel = row[0]
+                ylabel = row[1]
+
+
+            else:
+                x.append(float(row[0]))
+                y.append(float(row[1]))
+
+        return x, y, xlabel, ylabel
+
+
+x, y, xlabel, ylabel = open_file('Jacob Kahn - time_and_temp.txt')
+
+                 #############
+################## PROBLEM 1 ##################
+                 #############
+## PROBLEM 1a
+
+def plot_data(x, y, title, xlabel, ylabel, plot_code):
+    xmin = min(x)
+    xmax = max(x)
+
+    ymin = min(y)
+    ymax = max(y)
+
+    plt.title(f'{title}')
+
+    plt.xlabel = xlabel
+    plt.ylabel = ylabel
+
+    plt.plot(x, y, plot_code)
+
+    plt.axis([xmin, xmax, ymin, ymax])
+
+    plt.show()
+
+
+plot_data(x, y, 'KOBIS GRAPH', xlabel, ylabel, '--.r')
