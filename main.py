5f7cf7f3ec40524b7c188cff0b3331b685b68ac4
kobi <kobikahn@gmail.com>
2021-11-14 18:27:57 -0500

    finish 1

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def open_file(file_name):
             row = row.split()
             x_counter += 1
             if x_counter == 1:
-
                 xlabel = row[0]
                 ylabel = row[1]
 
@@ -22,15 +21,31 @@ def open_file(file_name):
 
         return x, y, xlabel, ylabel
 
+                                                                 #############
+                                                ################## PROBLEM 1 ##################
+                                                                 #############
 
-x, y, xlabel, ylabel = open_file('Jacob Kahn - time_and_temp.txt')
-
-                 #############
-################## PROBLEM 1 ##################
-                 #############
 ## PROBLEM 1a
 
 def plot_data(x, y, title, xlabel, ylabel, plot_code):
+    counter = 0
+
+    x_num = 0
+    y_num = 0
+
+    for int_x in x:
+        counter += 1
+        x_num += int_x
+
+    for int_y in y:
+        y_num += int_y
+
+    x_avg = (x_num / counter)
+    y_avg = (y_num / counter)
+
+    centroid = [x_avg, y_avg]
+    print(centroid)
+
     xmin = min(x)
     xmax = max(x)
 
@@ -44,9 +59,26 @@ def plot_data(x, y, title, xlabel, ylabel, plot_code):
 
     plt.plot(x, y, plot_code)
 
+    plt.text(centroid[0], centroid[1], 'Data Centroid')
+
+    # plt.hlines(xmin, xmax, centroid[1])
+    # plt.vlines(ymin, ymax, centroid[0])
+    ######### ASK ABOUT HOW TO DO HORIZONTAL AND VERTICAL LINES
+
     plt.axis([xmin, xmax, ymin, ymax])
 
     plt.show()
 
 
-plot_data(x, y, 'KOBIS GRAPH', xlabel, ylabel, '--.r')
+## 1b
+# x, y, xlabel, ylabel = open_file('Jacob Kahn - time_and_temp.txt')
+# plot_data(x, y, 'KOBI\n GRAPH', xlabel, ylabel, '--.r')
+
+#### 2
+# x, y, xlabel, ylabel = open_file('Jacob Kahn - temp_and_pressure.txt')
+# plot_data(x, y, 'KOBIS GRAPH', xlabel, ylabel, 'or')
+
+###### 3
+x, y, xlabel, ylabel = open_file('Jacob Kahn - time_temp_pressure_volume.txt')
+
+
